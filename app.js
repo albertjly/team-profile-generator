afc5e86deb5679bff979012c99731fa3b8e635f6
Albert Jiang <albertjly@gmail.com>
2021-04-20 19:21:05 -0700

    almost done

--- app.js
+++ app.js
@@ -1,179 +1,156 @@
-const Employee = require('./lib/Employee');
+const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
-const templateHelper = require('./src/template_helper');
 const fs = require('fs');
-let manager = new Manager('Albert', 5, 'eee@lll.com', 555);
-let managerCard = `<div class="col-4">
-            <div class="top">
-                <h2 class="name">${manager.getName()}</h2>
-                <h3 class="title"><i class="far fa-id-card"></i>${manager.getRole()}</h3>
-            </div>
-            <ul class="detail">
-                <li><p class="id">ID: ${manager.getId()}</p></li>
-                <li><p class="email">Email: ${manager.getEmail()}</p></li>
-                <li><p class="role">office No.:${manager.getOfficeNumber()}</p></li>
-            </ul>
-        </div>`;
-let htmlData = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Team Profile Generator</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
-    <style>
-        *{
-            margin: 0;
-            padding: 0;
-        }
-        ul, ol{
-            list-style: none;
-        }
-        body{
-            font-family: Arial, Helvetica, sans-serif;
-        }
-        a{
-            color: rgb(122, 93, 19);
-            font-weight: bold;
-            text-decoration: none;
-        }
-        header{
-            height: 200px;
-            background-color: #e9b651;
-            color: white;
-            line-height: 200px;
-            text-align: center;
-            font-size: 6vh;
-        }
-        .container{
-            height: 1000px;
-            margin-top: 20px;
-        }
-        .row{
-            margin: 0 auto;
-            width: 70%;
-            display: flex;
-            justify-content: space-evenly;
-            flex-wrap: wrap;
-        }
-        .col-4{
-            width: 260px;
-            height: 320px;
-            background-color: #fff;
-            margin: 10px;
-            border-radius: 5px;
-            box-shadow: 3px 3px 8px black;
-            overflow: hidden;
-        }
-        .col-4:hover{
-            transform: scale(1.01);
-            cursor: pointer;
-        }
-        .top{
-            height: 35%;
-            background-color: #c57c1d;
-            color: #eee;
-            display: flex;
-            padding-left: 10%;
-            flex-direction: column;
-            justify-content: center;
-        }
-        .name{
-            font-size: 3vh;
-        }
-        .title{
-            margin-top: 2%;
-            font-size: 2.3vh;
-            opacity: .9;
-        }
-        .title i{
-            margin-right: 6px;
-        }
-        .detail{
-            margin-top: 38px;
-        }
-        .detail li{
-            margin: 0 auto;
-            width: 86%;
-            /*background-color: #eee;*/
-            border-top: 1px solid #bbb;
-            border-left: 1px solid #bbb;
-            border-right: 1px solid #bbb;
-            height: 5vh;
-        }
-        .detail li:first-child{
-            border-top-left-radius: 5px;
-            border-top-right-radius: 5px;
-        }
-        .detail li:last-child{
-            border-bottom: 1px solid #bbb;
-            border-bottom-left-radius: 5px;
-            border-bottom-right-radius: 5px;
-        }
-        .detail li p{
-            line-height: 5vh;
-            padding-left: 10px;
-        }
-    </style>
-</head>
-<body>
-<header>My Team</header>
-<div class="container">
-    <div class="row">
-        <div class="col-4">
-            <div class="top">
-                <h2 class="name">${manager.name}</h2>
-                <h3 class="title"><i class="fab fa-github"></i>Engineer</h3>
-            </div>
-            <ul class="detail">
-                <li><p class="id">ID: 1</p></li>
-                <li><p class="email">Email: ddd_ddd@gggg.com</p></li>
-                <li><p class="role">GitHub: <a href="https://github.com/nicolasawesome">nicolasawesome</a></p></li>
-            </ul>
-        </div>
-        ${managerCard}
-        <div class="col-4">
-            <div class="top">
-                <h2 class="name">Rocket Man</h2>
-                <h3 class="title"><i class="fas fa-rocket"></i>Intern</h3>
-            </div>
-            <ul class="detail">
-                <li><p class="id">333</p></li>
-                <li><p class="email">333</p></li>
-                <li><p class="role">333</p></li>
-            </ul>
-        </div>
-        <div class="col-4">
-            <div class="top">
-                <h2 class="name">Elizabeth</h2>
-                <h3 class="title"><i class="far fa-id-card"></i>Employee</h3>
-            </div>
-            <ul class="detail">
-                <li><p class="id">333</p></li>
-                <li><p class="email">333</p></li>
-                <li><p class="role">333</p></li>
-            </ul>
-        </div>
-    </div>
-</div>
+const path = require('path');
+const getHtml = require('./src/template_helper');
+const employeeArr = [];
 
-</body>
-</html>`;
+const questionManagerArr = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the team manager's name?",
+        validate: input => !!input
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the team manager's id?",
+        validate: input => !!input
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the team manager\'s address?',
+        validate: input => !!input
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is the team manager\'s office number?',
+        validate: input => !!input
+    }
+];
 
-fs.writeFile('./dist/index.html', htmlData, err => {
-    if (err) throw new Error(err);
-    console.log('Done!');
-});
+const questionEngineerArr = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your engineer's name?",
+        validate: input => !!input
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your engineer's id?",
+        validate: input => !!input
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your engineer\'s address?',
+        validate: input => !!input
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'What is your engineer\'s GitHub account?',
+        validate: input => !!input
+    }
+];
 
-readFile(`${manager.getRole()}-${manager.getName()}`);
+const questionInternArr = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your intern's name?",
+        validate: input => !!input
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your intern's id?",
+        validate: input => !!input
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your intern\'s address?',
+        validate: input => !!input
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is your intern\'s school?',
+        validate: input => !!input
+    }
+];
 
-function readFile(fileName) {
-    fs.readFile(`./src/${fileName}.json`, 'utf-8',(err, data) => {
-        if (err) throw new Error(err);
-        console.log(data);
-    });
+const roleArr = [
+    {
+        type: 'list',
+        name: 'role',
+        message: 'Which type of team member would you like to add?',
+        choices: [
+            'Manager',
+            'Engineer',
+            'Intern',
+            'I don\'t want to add any more team members'
+        ],
+    }
+];
+console.log('Please build your team.');
 
+function init() {
+    inquirer
+        .prompt(roleArr)
+        .then(
+            data => {questionLoop(data)}
+        );
 }
+
+function questionLoop(data) {
+    if (data.role === 'Manager'){
+        return inquirer
+            .prompt(questionManagerArr)
+            .then(({name, id, email, officeNumber}) => {
+                // data.role = 'Manager';
+                const employee = new Manager(name, id, email, officeNumber);
+                pushAndLoop(employee)
+            });
+    }else if (data.role === 'Engineer'){
+        return inquirer
+            .prompt(questionEngineerArr)
+            .then(({name, id, email, gitHub}) => {
+                // data.role = 'Manager';
+                const employee = new Engineer(name, id, email, gitHub);
+                pushAndLoop(employee)
+            });
+    }else if (data.role === 'Intern') {
+        return inquirer
+            .prompt(questionInternArr)
+            .then(({name, id, email, school}) => {
+                // data.role = 'Manager';
+                const employee = new Intern(name, id, email, school);
+                pushAndLoop(employee)
+            });
+    }else {
+        // console.log(employeeArr);
+        const htmlData = getHtml(employeeArr);
+        fs.writeFile(path.join(__dirname, './dist/index.html'), htmlData, err => {
+            if (err) throw new Error(err);
+            console.log('Done!');
+        });
+    }
+}
+
+function pushAndLoop(data) {
+    // console.log(data);
+    // writeToFile(`${data.role}-${data.name}.json`, JSON.stringify(data));
+    employeeArr.push(data);
+    init();
+}
+
+init();
--- src/template_helper.js
+++ src/template_helper.js
@@ -1,155 +1,58 @@
-const inquirer = require('inquirer');
-const Employee = require('../lib/Employee');
-const Engineer = require('../lib/Engineer');
-const Manager = require('../lib/Manager');
-const Intern = require('../lib/Intern');
-const fs = require('fs');
-
-const questionManagerArr = [
-    {
-        type: 'input',
-        name: 'name',
-        message: "What is the team manager's name?",
-        validate: input => !!input
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "What is the team manager's id?",
-        validate: input => !!input
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the team manager\'s address?',
-        validate: input => !!input
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'What is the team manager\'s office number?',
-        validate: input => !!input
+function generateCard(employee) {
+    let special, role;
+    switch (employee.getRole()) {
+        case 'Manager':
+            role = `<i class="fas fa-glasses"></i>`;
+            special = `office No.: ${employee.getOfficeNumber()}`;
+            break;
+        case 'Engineer':
+            role = `<i class="fab fa-github"></i>`;
+            special = `GitHub: ${employee.getGitHub()}`;
+            break;
+        case 'Intern':
+            role = `<i class="far fa-id-card"></i>`;
+            special = `School: ${employee.getSchool()}`;
+            break;
     }
-];
 
-const questionEngineerArr = [
-    {
-        type: 'input',
-        name: 'name',
-        message: "What is your engineer's name?",
-        validate: input => !!input
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "What is your engineer's id?",
-        validate: input => !!input
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your engineer\'s address?',
-        validate: input => !!input
-    },
-    {
-        type: 'input',
-        name: 'gitHub',
-        message: 'What is your engineer\'s GitHub account?',
-        validate: input => !!input
-    }
-];
-
-const questionInternArr = [
-    {
-        type: 'input',
-        name: 'name',
-        message: "What is your intern's name?",
-        validate: input => !!input
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "What is your intern's id?",
-        validate: input => !!input
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your intern\'s address?',
-        validate: input => !!input
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'What is your intern\'s school?',
-        validate: input => !!input
-    }
-];
-
-const roleArr = [
-    {
-        type: 'list',
-        name: 'role',
-        message: 'Which type of team member would you like to add?',
-        choices: [
-            'Manager',
-            'Engineer',
-            'Intern',
-            'I don\'t want to add any more team members'
-        ],
-    }
-];
-console.log('Please build your team.');
-
-function init() {
-    inquirer
-        .prompt(roleArr)
-        .then(
-            data => {questionLoop(data)}
-        );
+    return `<div class="col-4">
+    <div class="top">
+        <h2 class="name">${employee.getName()}</h2>
+        <h3 class="role">${role}${employee.getRole()}</h3>
+    </div>
+    <ul class="detail">
+        <li><p class="id">ID: ${employee.getId()}</p></li>
+        <li><p class="email">Email: ${employee.getEmail()}</p></li>
+        <li><p class="special">${special}</p></li>
+    </ul>
+</div>`
 }
 
-function questionLoop(data) {
-    if (data.role === 'Manager'){
-        return inquirer
-            .prompt(questionManagerArr)
-            .then(data => {
-                data.role = 'Manager';
-                pushAndLoop(data)
-            });
-    }else if (data.role === 'Engineer'){
-        return inquirer
-            .prompt(questionEngineerArr)
-            .then(data => {
-                data.role = 'Engineer';
-                pushAndLoop(data)
-            });
-    }else if (data.role === 'Intern') {
-        return inquirer
-            .prompt(questionInternArr)
-            .then(data => {
-                data.role = 'Intern';
-                pushAndLoop(data)
-            });
-    }else {
-        // console.log(employeeArr.array);
-        return false;
-    }
+function getHtml (employeeArr) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Team Profile Generator</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+        <header>My Team</header>
+        <div class="container">
+            <div class="row">
+                ${employeeArr.map(employee => generateCard(employee)).join('')}
+            </div>
+        </div>
+    
+    </body>
+    
+    </html>`
 }
 
-function pushAndLoop(data) {
-    // console.log(data);
-    writeToFile(`${data.role}-${data.name}.json`, JSON.stringify(data));
-    init();
-}
 
-function writeToFile(filename, data) {
-    fs.writeFileSync(`./src/${filename}`, data, err => {
-        if (err) throw new Error(err);
-        console.log('Done!');
-    });
-}
-
-// console.log(employeeArr.array);
-module.exports = init();
-// exports.employee = employeeArr;
+module.exports = getHtml;
